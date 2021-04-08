d10ef5404f7d0243e5f08759035d8fc653e95385
Jacob Nguyen <jvnguyen@cs.ualberta.ca>
2021-04-08 14:09:56 -0600

    minor change

--- A5T2.py
+++ A5T2.py
@@ -1,43 +1,37 @@
 """ Task 2: Write a Python application (A5T2.py) which will use a MongoDB database called A5db (which will be embedded in your own MongoDB data repository) and create within such database a single collection where all the reviews associated to one given listing are to be embedded within that one listing. The input to the application should be .csv files provided above (they can be hard-coded within your applications) but there will be no tangible output. (Note: our lab installations lack mongoexport and mongoimport applications, which could otherwise be used to "dump" and "upload" MongoDB collections, respectively.) """
-
-""" 
-In order to be sure that your ETL process worked as expected you can use the following queries as a (admittedly very simple) "checksum" verification. 
-
-For the MongoDB database:
-
-db.listings.aggregate([ 
-
-    {$group: {_id: null, min: {$min: "$host_id"}, max: {$max: "$host_id"}, avg: {$avg: "$host_id"}, count: {$sum: 1}}} 
-
-])
-
-should return 
-
-{ "_id" : null, "min" : 6033, "max" : 387534175, "avg" : 115176061.85829493, "count" : 4340 }
-
-and
-
-db.listings.aggregate([
-
-    {$unwind: "$reviews"}, 
-
-    {$group: {_id: null, min: {$min: "$reviews.id"}, max: {$max: "$reviews.id"}, avg: {$avg: "$reviews.id"}, count: {$sum: 1}}}
-
-])
-
-should return 
-
-{ "_id" : null, "min" : 26444, "max" : 730124064, "avg" : 370354766.84915775, "count" : 147936 }
-"""
-
+import pymongo
 from pymongo import MongoClient 
 import pandas as pd
-import json
+import json, csv,sys, getopt, pprint
 
 def main():
-
-	client = MongoClient()
+	'''
+	client = MongoClient('localhost', 27017)
 
 	database = client["A5db"]
+
 	collection = database["listings"]
+
 	collection.delete_many({})
+	
+	csvfile = open("YVR_Airbnb_listings_summary.csv", "r")
+	reader = csv.DictReader(csvfile)
+
+	header = ["id", "name", "host_id", "host_name", "neighbourhood", "room_type", "price", "minimum_nights", "availability_365"]
+	'''
+	with open('YVR_Airbnb_listings_summary.csv', 'r', encoding='utf8') as file:
+		data = file.read()
+		client = MongoClient()
+		database = client["A5db"]
+		database.segment.drop()
+		header = ["id", "name", "host_id", "host_name", "neighbourhood", "room_type", "price"    , "minimum_nights", "availability_365"]
+
+		for each in data:
+			row = {}
+			for field in header:
+				row[field]=each[field]
+			db.segment.insert(row)
+
+	database.eval("db.shutdownServer()")
+		
+main()
