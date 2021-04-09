c971b532003c490747e7b2952b895bbb9b2beb17
Jacob Nguyen <jvnguyen@cs.ualberta.ca>
2021-04-08 19:56:34 -0600

    updated

--- A5T2.py
+++ A5T2.py
@@ -1,7 +1,7 @@
 """ Task 2: Write a Python application (A5T2.py) which will use a MongoDB database called A5db (which will be embedded in your own MongoDB data repository) and create within such database a single collection where all the reviews associated to one given listing are to be embedded within that one listing. The input to the application should be .csv files provided above (they can be hard-coded within your applications) but there will be no tangible output. (Note: our lab installations lack mongoexport and mongoimport applications, which could otherwise be used to "dump" and "upload" MongoDB collections, respectively.) """
 import pymongo
 from pymongo import MongoClient
-
+import csv
 def main():
 
 	client = MongoClient('localhost', 27017)
@@ -10,9 +10,19 @@ def main():
 
 	collection = database["listings"]
 	
-	csvfile = open("YVR_Airbnb_listings_summary.csv", "r")
-	csvfile.close()
+	csvfilelistings = open("YVR_Airbnb_listings_summary.csv", "r", encoding='utf8')
+	csvfilereviews = open("YVR_Airbnb_reviews.csv", "r", encoding='utf8')
 
-	header = ["id", "name", "host_id", "host_name", "neighbourhood", "room_type", "price", "minimum_nights", "availability_365"]
+	listingsrows = csv.reader(csvfilelistings)
+	next(listingsrows)
+	listings = collection.insert_many(listingsrows)
+
+	reviewsrows = csv.reader(csvfilereviews)
+	next(reviewsrows)
+	reviews = collection.insert_many(listingsrows)
+	
+	
+	csvfilelistings.close()
+	csvfilereviews.close()
 	client.close()
 main()
