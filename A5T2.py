832862fc25a932bc334120dfdc20c34db53386f3
kroule <roulette@ualberta.ca>
2021-04-11 04:51:08 -0600

    Task 2 is complete, should be able to finish the rest pretty easily now

--- A5T2.py
+++ A5T2.py
@@ -4,7 +4,10 @@ from pymongo import MongoClient
 import csv
 def main():
 
-	client = MongoClient('localhost', 27008)
+	# I may forget to change this back, apologies
+
+	#client = MongoClient('localhost', 27008)
+	client = MongoClient('localhost', 27017)
 
 	database = client["A5db"]
 
@@ -18,19 +21,96 @@ def main():
 	listingsfile = open('YVR_Airbnb_listings_summary.csv', 'r', encoding= 'utf8')
 	reviewsfile = open('YVR_Airbnb_reviews.csv', 'r', encoding = 'utf8')
 
+
+	### Create an object that operates like a regular reader but maps the information in each row to a dict whose keys are given by the optional fieldnames parameter.
+	### The fieldnames parameter is a sequence. If fieldnames is omitted, the values in the first row of file f will be used as the fieldnames. 
+	### Regardless of how the fieldnames are determined, the dictionary preserves their original ordering.
+
 	listingsrows = csv.DictReader(listingsfile)
 	reviewsrows = csv.DictReader(reviewsfile)
 
-	header = ["id", "name", "host_id", "host_name", "neighbourhood", "room_type", "price", "minimum_nights", "availability_365"]
+	### Didn't know a better way to do this - if a given field name needs to be an 'int' it's set to true
+	header_listings = {"id":True, "name":False, "host_id":True, "host_name":False, "neighbourhood":False, "room_type":False, "price":True, "minimum_nights":True, "availability_365":True}
+	header_reviews = {'listing_id':True,'id':True,'date':False,'reviewer_id':True,'reviewer_name':False,'comments':False}
 	
-
-	
+	### Step 1: Initialize a dictionary for every listings 'reviews' field (this will be a list of the reviews)
+	group_reviews = {}
 	for rows in listingsrows:
+		for i, v in header_listings.items():
+			### Check if field should be casted to int
+			if(v == True):
+				rows[i] = int(rows[i])
+		group_reviews[str(rows['id'])] = []
+		
+	### Step 2: Add the reviews to the list that belong to the proper listings
+	### rows['listing_id'] = this is a row in the 'reviews' table with the 'listing_id' = to the 'id' within the table listings
+	for rows in reviewsrows:
+		for i, v in header_reviews.items():
+			### Check if field should be casted to int
+			if(v == True):
+				rows[i] = int(rows[i])
+				
+		group_reviews[str(rows['listing_id'])].append(rows)
+		
+	### Set the file reader file pointer back to the start
+	reviewsfile.seek(0)
+	listingsfile.seek(0)
+	### Skip the first row to ignore the header
+	next(listingsrows)
+	next(reviewsrows)
+		
+	### Iterate over every listing
+	for rows in listingsrows:
+		
+		### Add the appropriate fields into the dictionary
 		row = {}
-		for field in header:
-			row[field] = rows[field]
-		collection.insert_one(row)
+		for field in listingsrows.fieldnames:
+			### Check if the field should be casted to int
+			if(header_listings[field]):
+				row[field] = int(rows[field])
+			else:
+				row[field] = rows[field]
+			
 	
+		### Add our new 'reviews' column/field to the listing
+		row['reviews'] = group_reviews[str(rows['id'])]
+				
+		### Add our new entry into the listings dict
+		collection.insert_one(row)
+		
+	### All done!
 	client.close()
+	
+	### This builds the following structure, Please verify this works okay!
+	#listings 
+	#[ 
+	  #{
+	  #_id: ObjectId("6072b8569c7e8809eedc60d8"),
+	  #id: 18589,
+	  #name: 'Commercial Drive B&B',
+	  #host_id: 71508,
+	  #host_name: 'Sylvain & Alexis',
+	  #neighbourhood: 'Grandview-Woodland',
+	  #room_type: 'Private room',
+	  #price: 79,
+	  #minimum_nights: 1,
+	  #availability_365: 194,
+	  #reviews: [ {listing_id: 18589,
+	              #id: 300387,
+	              #date: '2011-06-06',
+	              #reviewer_id: 578644,
+	              #reviewer_name: 'Bonita',
+	              #comments: 'Sylvain is a charming host and an extremely talented and creative cook! Our stay in Vancouver was made 100% better because of our choice of this quiet, beautifully decorated home, combined with the truly gracious and generous nature of Sylvain and his partner. We will return to stay with them in the future when Vancouver beckons again.'
+	              #},
+	             #{listing_id: 18589,
+	              #id: 308845,
+	              #date: '2011-06-11',
+	              #reviewer_id: 667825,
+	              #reviewer_name: 'Kim',
+	              #comments: 'What a wonderful experience staying with this lovely couple! And wow, what great breakfasts they provided for us! They even bought special cereal for our two small children:) Very comfortable and cozy accommodations. \n' +
+	              #'I highly recommend this bnb!' }
+	             #]
+	  #}
+	#]
 
 main()
--- readme.txt
+++ readme.txt
@@ -1,3 +1,7 @@
+## Remove after ##
+A single .tgz file containing a README.txt file, and all necessary (and commented) source code in Python necessary to execute the necessary applications (the naming of all files must be as described above). The README file should have the name(s) of student(s), their ccid(s), the usual collaboration statement, a list of all included files, plus a brief instructions on how to run the applications. The README file should also include a section with a simple "how to” guide for executing the applications for each task. The .tgz file (containing at least 12 files, README.txt plus 11 .py files and other files with common functions you have defined) should be submitted using this submission page. 
+##################
+
 Name: Jacob Nguyen, Keaton Roulette, Liam Gyori
 ccid: jvnguyen, roulette, lgyori
 collaboration statement: The above members worked on the project together
