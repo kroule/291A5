4892caa1656f892e8f7afa876983ce9f0d9f4521
lgyori2 <35353343+lgyori2@users.noreply.github.com>
2021-04-13 13:19:27 -0600

    Add files via upload

---
+++ A5T3SQLite.py
@@ -0,0 +1,39 @@
+#Application 5 Task 3 
+#Find how many listings each host own, ordering the output by host_id and only output the top 10.
+
+import sqlite3
+
+connection = None
+cursor = None
+
+A5 = []
+
+
+############################ START OF QUERY #########################
+def runQuery():
+        global connection, cursor
+        
+        #SQL STATEMENT: select the number of times the host id appears as well as the host id and then group it by the host id and limit the results to 10.
+        Query = """SELECT count(host_id), host_id FROM listings GROUP BY host_id LIMIT 10 """
+        connection = sqlite3.connect('A5.db')
+        cursor = connection.cursor()
+        #execute the query with no runtime input as no input is required
+        cursor.execute(Query)
+        #fetch all of the results from the query and assign them to myresult
+        myresult = cursor.fetchall()
+        #for all results in myresult print each element of said results
+        for x in myresult:
+                print(x)
+        #commit the connection
+        connection.commit()
+        #close the connection
+        connection.close()
+
+############################## END OF QUERY ################################
+
+
+#main function will run function query and return results
+def main():
+        global connection, cursor
+        runQuery()
+main()
