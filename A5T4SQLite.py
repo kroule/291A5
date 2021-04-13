3f6b97e7c02e73e833bfe92b8ea9d4ae91dc8388
lgyori2 <35353343+lgyori2@users.noreply.github.com>
2021-04-13 17:22:14 -0600

    3, 4, 5, 8, 9,

---
+++ A5T4SQLite.py
@@ -0,0 +1,41 @@
+#Application 5 task 4
+#Find which listed property(ies) has(have) not received any review, order them by listing_id and output the top 10? 
+
+
+import sqlite3
+import time
+connection = None
+cursor = None
+start_time = time.time()
+
+A5 = []
+
+
+######################### START OF QUERY ############################
+def runQuery():
+        global connection, cursor
+        
+        #SQL STATEMENTS: select the id from listings where the id is not found in the listing_id from reviews and limit results to 10
+        Query = """SELECT id FROM listings WHERE id NOT IN (SELECT listing_id FROM reviews) GROUP BY id LIMIT 10 """
+        connection = sqlite3.connect('A5.db')
+        cursor = connection.cursor()
+        #execute the query with no runtime input as no input is required
+        cursor.execute(Query)
+        #fetch all of the results from the query and assign them to myresult
+        myresult = cursor.fetchall()
+        #for all results in myresult print each element of said results
+        for x in myresult:
+                print(x)
+        #commit the connection
+        connection.commit()
+        #close the connection
+        connection.close()
+
+############################## END QUERY ###########################
+
+#main() will run the query and print the results
+def main():
+        global connection, cursor
+        runQuery()
+main()
+print("Program runtime:  %s seconds" % (time.time() - start_time))
