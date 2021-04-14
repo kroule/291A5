f0062959a25693bfa1dc6aefd79a0af17e540f2f
lgyori2 <35353343+lgyori2@users.noreply.github.com>
2021-04-14 17:14:56 -0600

    3, 4, 5, 8 9

---
+++ A5T9MongoDB.py
@@ -0,0 +1,42 @@
+  
+""" Find the top 3 listings which have reviews most similar to a set of keywords given at run-time (e.g., using command line prompt or via an application parameter). Assume those keywords will be given in a comma separated string such as "nice, inexpensive, quiet".  """
+
+from pymongo import MongoClient
+import time 
+
+client = MongoClient()
+db = client["A5db"]
+start_time = time.time()
+
+def runQuery():
+    # Create or open the collection in the db
+    listings_collection = db["listings"]
+    user = str(input('enter keywords: '))
+    """ Find the top 3 listings which have reviews most similar to a set of keywords given at run-time (e.g., using command line prompt or via an application parameter). Assume those keywords will be given in a comma separated string such as "nice, inexpensive, quiet".  """
+    index_name = listings_collection.create_index( [("reviews.comments", "text")] )
+    results = listings_collection.find ({ "reviews.comments" : { "$regex" : user } }).limit(3)
+    for row in results:
+        for i, v in row.items():
+            if(type(v) == str):
+                print(v.encode("utf-8"))
+            elif ( type(v) == list ):
+                for j in v:
+                    for k, l in j.items():
+                        if( type(l) == str ):
+                            print(l.encode("utf-8"))
+                        else:
+                            print(l)
+            else:
+                print(i + str(v))
+
+
+def main():
+    # List collection names.
+    collist = db.list_collection_names()
+    if "listings" in collist:
+        print("The collection exists.")
+        
+    runQuery()
+
+main()
+print("Program runtime:  %s seconds" % (time.time() - start_time))
